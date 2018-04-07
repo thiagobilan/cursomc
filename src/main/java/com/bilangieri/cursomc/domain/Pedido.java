d1f1a8a77656084677ecd3e4359d271f842cd6b5
Thiago Bilangieri <thiagobilan@yahoo.com.br>
2018-04-07 07:43:40 -0300

    Criado o Endpoint /pedidos

--- src/main/java/com/bilangieri/cursomc/domain/Pedido.java
+++ src/main/java/com/bilangieri/cursomc/domain/Pedido.java
@@ -15,36 +15,40 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 @Entity
-public class Pedido implements Serializable{
+public class Pedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	
+	@JsonFormat(pattern="dd/MM/yyyy HH:mm")
 	private Date instante;
-	
-	@OneToOne(cascade = CascadeType.ALL, mappedBy="pedido")
+
+	@JsonManagedReference
+	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	
+
+	@JsonManagedReference
 	@ManyToOne
-	@JoinColumn(name="cliente_id")
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
-	
+
 	@ManyToOne
-	@JoinColumn(name="endereco_de_entrega_id")
+	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
 
-	//private List<Produto> itens = new ArrayList<>();
-	
-	@OneToMany(mappedBy="id.pedido")
-	private Set<ItemPedido>itens = new HashSet<>();
-	
-	public Pedido() {
-		
-	}
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 
+	public Pedido() {
+
+	}
 
 	public Pedido(Integer id, Date instante, Cliente cliente, Endereco enderecoDeEntrega) {
 		super();
@@ -54,66 +58,53 @@ public class Pedido implements Serializable{
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
-
 	public Integer getId() {
 		return id;
 	}
-
 
 	public void setId(Integer id) {
 		this.id = id;
 	}
 
-
 	public Date getInstante() {
 		return instante;
 	}
-
 
 	public void setInstante(Date instante) {
 		this.instante = instante;
 	}
 
-
 	public Pagamento getPagamento() {
 		return pagamento;
 	}
-
 
 	public void setPagamento(Pagamento pagamento) {
 		this.pagamento = pagamento;
 	}
 
-
 	public Cliente getCliente() {
 		return cliente;
 	}
-
 
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
 
-
 	public Endereco getEnderecoDeEntrega() {
 		return enderecoDeEntrega;
 	}
 
-
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
-	
+
 	public Set<ItemPedido> getItens() {
 		return itens;
 	}
 
-
 	public void setItens(Set<ItemPedido> itens) {
 		this.itens = itens;
 	}
-	
-
 
 	@Override
 	public int hashCode() {
@@ -122,7 +113,6 @@ public class Pedido implements Serializable{
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -141,8 +131,4 @@ public class Pedido implements Serializable{
 		return true;
 	}
 
-
-
-	
-	
 }
---
+++ src/main/java/com/bilangieri/cursomc/services/PedidoService.java
@@ -0,0 +1,27 @@
+package com.bilangieri.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bilangieri.cursomc.domain.Pedido;
+import com.bilangieri.cursomc.repositories.PedidoRepository;
+import com.bilangieri.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	
+	public Pedido buscar(Integer id) {
+		
+		Optional<Pedido> obj = repo.findById(id);
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto nao Encontrado! Id: " + id + ", Tipo: " + Pedido.class.getName()));
+		
+	}
+
+}
